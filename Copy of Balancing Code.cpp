00c4eb2ed1c9029af14d22b5b2ad57957917f32f
TimmyTwoD <david12345l@gmail.com>
2023-03-23 19:28:50 -0700

    Finish Sift and counting time 99% done

--- Copy of Balancing Code.cpp
+++ Copy of Balancing Code.cpp
@@ -5,16 +5,22 @@
 #include <set>
 #include <queue>
 #include <utility> 
+#include <fstream>
+#include <numeric> 
+#include <cmath>
+
+int CraneCost = 0;
 
 struct container{
     double weight;
     std::string name;
 };
 
-container* newContainer(double weight)
+container* newContainer(double weight, std::string name)
 {
     container* newContainer = new container();
     newContainer->weight = weight;
+    newContainer->name = name;
     return newContainer;
 }
 
@@ -53,7 +59,15 @@ struct Heuristic // comparator class for constructing the priority queue
 {
     bool operator()(const Node*a, const Node* b)
     {
-        return ((a->cost + a->smallestDifference) > (b->cost+ b->smallestDifference)); 
+        return ((a->cost) >= (b->cost)); 
+    }
+};
+
+struct WeightHeuristic // comparator class for sift
+{
+    bool operator()(const container*a, const container* b)
+    {
+        return ((a->weight) < (b->weight)); 
     }
 };
 
@@ -85,20 +99,36 @@ void printGrid(Node* shipGrid)
     std::cout<<"\n";
 }
 
-void printAnswer(Node* shipGrid)
+void printAnswer(Node* shipGrid, std::vector<std::string> &answerPath)
 {
+    std::string temp;
+
     if (shipGrid != nullptr)
     {
-        printAnswer(shipGrid->previous);
+        printAnswer(shipGrid->previous, answerPath);
         if (shipGrid->previous != nullptr)
         {
-            std::cout<<"Move container at ("<<shipGrid->beforeCoords.second<<","<<shipGrid->beforeCoords.first<<") to ("<<shipGrid->currentCoords.second<<","<<shipGrid->currentCoords.first<<")\n\n";
+            temp += "Move (";
+            temp += std::to_string(shipGrid->beforeCoords.second);
+            temp +=",";
+            temp += std::to_string(shipGrid->beforeCoords.first);
+            temp += ") to (";
+            temp += std::to_string(shipGrid->currentCoords.second);
+            temp +=",";
+            temp += std::to_string(shipGrid->currentCoords.first);
+            temp += ")";
+            //std::cout<<temp<<"\n\n";
+            //std::cout<< "\n"<<std::abs(shipGrid->previous->currentCoords.first - shipGrid->beforeCoords.first) + std::abs(shipGrid->previous->currentCoords.second - shipGrid->beforeCoords.second);
+            //std::cout<<"\n"<<shipGrid->previous->currentCoords.first<<" and "<< shipGrid->beforeCoords.first<<std::endl;
+            CraneCost += std::abs(shipGrid->beforeCoords.first - shipGrid->currentCoords.first) + std::abs(shipGrid->beforeCoords.second - shipGrid->currentCoords.second);
+            answerPath.push_back(temp);
+            //std::cout<<"Move ("<<shipGrid->beforeCoords.second<<","<<shipGrid->beforeCoords.first<<") to ("<<shipGrid->currentCoords.second<<","<<shipGrid->currentCoords.first<<")\n\n";
         }
         else
         {
-            std::cout<<"Start\n\n";
+            //std::cout<<"Start\n\n";
         }
-        printGrid(shipGrid);
+        //printGrid(shipGrid);
     }
 }
 
@@ -160,20 +190,150 @@ int getTopofColumn(Node* shipGrid, int whichColumn)
         }
     }
     return 7;
-
 }
 
-void makeBalanced(Node* shipGrid)  
+bool isSiftDone(Node* shipGrid, Node* endstate)
 {
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            if (shipGrid->CurrentGrid[i][j]->name != endstate->CurrentGrid[i][j]->name)
+            {
+                return false;
+            }
+        }   
+        
+    }
+    return true;
+}
+
+Node* calculateEndState(Node* shipGrid)
+{
+    std::priority_queue<container*, std::vector<container*>, WeightHeuristic> queue;
+    container* temp;
+    Node* tempNode = shipGrid;
+    bool left = true;
+    
+    for (int i = 0; i < tempNode->CurrentGrid.size(); i++)
+    {
+        for (int j = 0; j < tempNode->CurrentGrid[i].size(); j++)
+        {
+            if (tempNode->CurrentGrid[i][j]->weight > 0)
+            {
+                temp = tempNode->CurrentGrid[i][j];
+                queue.push(temp);
+                temp = newContainer(0, "UNUSED");
+                tempNode->CurrentGrid[i][j] = temp;
+            }
+        }
+    }
+
+    while (!queue.empty())
+    {
+        temp = queue.top();
+        queue.pop();
+        if (left == true)
+        {
+            for (int i = 7; i > -1; i--)
+            {
+                for (int j = 5; j > -1; j--)
+                {
+                    if (tempNode->CurrentGrid[i][j]->weight == 0)
+                    {
+                        tempNode->CurrentGrid[i][j] = temp;
+                        left = false;
+                        break;
+                    }
+                }
+
+                if (left == false)
+                {
+                    break;
+                }
+            }
+        }
+        else if (left == false)
+        {
+            for (int i = 7; i > -1; i--)
+            {
+                for (int j = 6; j < 12; j++)
+                {
+                    if (tempNode->CurrentGrid[i][j]->weight == 0)
+                    {
+                        tempNode->CurrentGrid[i][j] = temp;
+                        left = true;
+                        break;
+                    }
+                }
+                if (left == true)
+                {
+                    break;
+                }
+                
+            }
+        }
+
+    }
+
+    return tempNode;
+}
+
+int ManHatDis(Node* shipGrid, Node* endstate)
+{
+    int cost = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            if (shipGrid->CurrentGrid[i][j]->weight > 0)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    for (int h = 0; h < 12; h++)
+                    {
+                        if (shipGrid->CurrentGrid[i][j]->name == endstate->CurrentGrid[k][h]->name)
+                        {
+                            //std::cout<<i<<","<<j<<","<<k<<","<<h<<std::endl;
+                            cost+= std::abs(k-i) + std::abs(h-j);
+                        }
+                    }
+                }
+            }
+        }   
+       
+    }
+    //std::cout<<cost<<std::endl;
+    return cost;
+}
+
+void makeBalanced(Node* shipGrid, bool Sifting)  
+{
+    int first;
+    int second;
+    std::string firstCoord;
+    std::string secondCoord;
     int NodeYPosition;
     int NodeXPosition; 
     int size;
     int temp;
     bool Balanced = false;
     Node* tempNode;
+    Node* endstate = newNode(shipGrid->CurrentGrid, nullptr);
 
     int count = 0;
 
+    if (Sifting == true)
+    {
+        calculateEndState(endstate);
+    }
+
+    int SiftCost = ManHatDis(shipGrid, endstate);
+
+    std::ofstream outfile ("C:/Users/Living Room/Desktop/Instructions.txt");
+
+    std::vector<std::string> answerPath;
     std::set<std::vector<std::vector<double>>> dupeTrack;
     std::set<std::vector<std::vector<double>>>::iterator itr;
     std::vector<std::vector<double>> testOutput;
@@ -198,11 +358,50 @@ void makeBalanced(Node* shipGrid)
         shipGrid = queue.top();
         queue.pop();
 
-        if(isBalanced(shipGrid))
+        if (isSiftDone(shipGrid, endstate) && Sifting == true)
         {
-            printAnswer(shipGrid);
-            std::cout<<"\nEstimated cost = "<<shipGrid->cost<<" minutes\n";
-            std::cout<<"Ship is balanced Now\n\n";
+            //printGrid(shipGrid);
+            printAnswer(shipGrid, answerPath);
+            
+            firstCoord.assign(answerPath[0], 6, 1);
+            secondCoord.assign(answerPath[0], 8, 1);
+            
+            first = std::stoi(firstCoord);
+            second = std::stoi(secondCoord);
+            CraneCost += std::abs(9-first) + std::abs(1 - second);
+            CraneCost += std::abs(1 - shipGrid->currentCoords.first) + std::abs(9 - shipGrid->currentCoords.second);
+            CraneCost -= std::abs(0 - first) + std::abs(0 - second);
+            //std::cout<<"\nEstimated cost = "<<SiftCost + CraneCost<<" minutes\n\n";
+            outfile<<"Estimated cost = "<<SiftCost+CraneCost<<" minutes"<<std::endl;
+            //std::cout<<"Ship is balanced Now\n\n";
+            for (int i = 0; i < answerPath.size(); i++)
+            {
+                outfile<<answerPath[i]<<std::endl;
+            }
+            return;
+        }
+        else if(isBalanced(shipGrid))
+        {
+            printAnswer(shipGrid, answerPath);
+       
+            firstCoord.assign(answerPath[0], 6, 1);
+            secondCoord.assign(answerPath[0], 8, 1);
+
+            first = std::stoi(firstCoord);
+            second = std::stoi(secondCoord);
+            
+            CraneCost += std::abs(9-first) + std::abs(1 - second);
+            CraneCost += std::abs(1 - shipGrid->currentCoords.first) + std::abs(9 - shipGrid->currentCoords.second);
+            CraneCost -= std::abs(0 - first) + std::abs(0 - second);
+            //std::cout<<"\nEstimated cost = "<<shipGrid->cost+CraneCost<<" minutes\n\n";
+
+            outfile<<"Estimated cost = "<<shipGrid->cost+CraneCost<<" minutes"<<std::endl;
+
+            for (int i = 0; i < answerPath.size(); i++)
+            {
+                outfile<<answerPath[i]<<std::endl;
+            }
+            //std::cout<<"Ship is balanced Now\n\n";
             return;
         }
 
@@ -216,132 +415,210 @@ void makeBalanced(Node* shipGrid)
             {
                 NodeYPosition = 7;
             }
-        
-            for (int i = 0; i < 12; i++)
+
+
+            while(shipGrid->CurrentGrid[NodeYPosition][NodeXPosition]->weight == -1 && NodeYPosition > -1)//if there is negative ones
             {
-                if (i != NodeXPosition)
+                NodeYPosition--;
+            }
+
+            if (NodeYPosition > -1)
+            {
+                if(shipGrid->CurrentGrid[NodeYPosition][NodeXPosition]->weight > 0)
                 {
-                    temp = getTopofColumn(shipGrid, i);
-                    if (temp > 0)
+                    for (int i = 0; i < 12; i++)
                     {
-                        
-                        //std::cout<<"\nNode swap "<< NodeXPosition<<","<<NodeYPosition<<" with "<<i<<","<<temp<<" By putting "<<shipGrid->CurrentGrid[NodeYPosition][NodeXPosition]->weight<<" at position "<< i<<","<< temp<<"\n";
-
-                        swapContainers(shipGrid, temp, i, NodeYPosition, NodeXPosition);
-
-                        for (int i = 0; i < 8; i++)
+                        if (i != NodeXPosition)
                         {
-                            for (int j = 0; j < 12; j++)
+                            temp = getTopofColumn(shipGrid, i);
+                            if (temp > 0)
                             {
-                                DupeGrid[i][j] = shipGrid->CurrentGrid[i][j]->weight;
-                            }   
-                        }
+                                
+                                //std::cout<<"\nNode swap "<< NodeXPosition<<","<<NodeYPosition<<" with "<<i<<","<<temp<<" By putting "<<shipGrid->CurrentGrid[NodeYPosition][NodeXPosition]->weight<<" at position "<< i<<","<< temp<<"\n";
 
-                        size = dupeTrack.size();
-                        dupeTrack.insert(DupeGrid);
-                        if (!(size == dupeTrack.size()))//if it isn't a dupe
-                        {  
-                            tempNode = newNode(shipGrid->CurrentGrid, shipGrid);
-                            tempNode->cost = shipGrid->cost + getCost(temp, i, NodeYPosition, NodeXPosition);
-                            tempNode->smallestDifference = std::abs(leftsideSum(tempNode) - rightsideSum(tempNode));
-                            tempNode->currentCoords.first =  i + 1;
-                            tempNode->currentCoords.second = std::abs(7-temp) + 1;
-                            tempNode->beforeCoords.first = NodeXPosition+1;
-                            tempNode->beforeCoords.second = std::abs(7-NodeYPosition)+1; 
-                            queue.push(tempNode);
-                        }
+                                swapContainers(shipGrid, temp, i, NodeYPosition, NodeXPosition);
 
-                        swapContainers(shipGrid, temp, i, NodeYPosition, NodeXPosition);
-                        
+                                for (int i = 0; i < 8; i++)
+                                {
+                                    for (int j = 0; j < 12; j++)
+                                    {
+                                        DupeGrid[i][j] = shipGrid->CurrentGrid[i][j]->weight;
+                                    }   
+                                }
+
+                                size = dupeTrack.size();
+                                dupeTrack.insert(DupeGrid);
+                                if (!(size == dupeTrack.size()))//if it isn't a dupe
+                                {  
+                                    tempNode = newNode(shipGrid->CurrentGrid, shipGrid);
+                                    if (Sifting == false)
+                                    {
+                                        tempNode->cost = shipGrid->cost + getCost(temp, i, NodeYPosition, NodeXPosition);
+                                    }
+                                    else if (Sifting == true)
+                                    {
+                                        tempNode->cost = ManHatDis(tempNode, endstate);
+                                    }
+                                    tempNode->smallestDifference = std::abs(leftsideSum(tempNode) - rightsideSum(tempNode));
+                                    tempNode->currentCoords.first =  i + 1;
+                                    tempNode->currentCoords.second = std::abs(7-temp) + 1;
+                                    tempNode->beforeCoords.first = NodeXPosition+1;
+                                    tempNode->beforeCoords.second = std::abs(7-NodeYPosition)+1; 
+                                    //printGrid(tempNode);
+                                    queue.push(tempNode);
+                                }
+
+                                swapContainers(shipGrid, temp, i, NodeYPosition, NodeXPosition);
+                                
+                            }
+                        }
                     }
                 }
             }
-            
         }
 
     }
     
 }
 
+bool isSubsetSum(std::vector<double> check, int n, int sum)
+{
+   
+    if (sum == 0)
+    {
+        return true;
+    }
+
+    if (n == 0)
+    {
+        return false;
+    }
+
+    if (check[n - 1] > sum)
+    {
+        return isSubsetSum(check, n - 1, sum);
+    }
+    return isSubsetSum(check, n - 1, sum) || isSubsetSum(check, n - 1, sum - check[n - 1]);
+}
+
 int main()
 {
+    std::vector<double> check;
     std::vector<container*> temp;
     std::vector<std::vector<container*>> shipGrid;
     Node* tempNode;
+    double reduce = 0;
+    int lowerBound;
+    int upperBound;
+    int checksize;
+    bool performSift = true;
+    CraneCost = 0;
 
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 12; j++)
         {
-            temp.push_back(newContainer(0));
+            temp.push_back(newContainer(0, "UNUSED"));
         }   
         shipGrid.push_back(temp);
         temp.clear();
     }
 
-    shipGrid[7][0]->weight = -1;
+    std::string yCoord;
+    std::string xCoord;
+    std::fstream myFile;
+    std::string getWeight;
+    std::string name;
 
-    shipGrid[3][3]->weight = 50;
-    shipGrid[4][3]->weight = 50;
-    shipGrid[5][3]->weight = 10;
-    shipGrid[6][3]->weight = 1;
-    shipGrid[7][3]->weight = 1;
-    shipGrid[7][9]->weight = 50;
-    shipGrid[6][9]->weight = 50;
-
-    shipGrid[7][11]->weight = -1;
+    int posY;
+    int posX;
+    double weight;
     
-    /*{
-        {0,0,0,0,0,0,  0,0,0,0,0,0},
-        {0,0,0,0,0,0,  0,0,0,0,0,0},
-        {0,0,0,0,0,0,  0,0,0,0,0,0},
-        {0,0,0,50,0,0,  0,0,0,0,0,0},
-        {0,0,0,50,0,0,  0,0,0,0,0,0},
-        {0,0,0,10,0,0,  0,0,0,0,0,0},
-        {0,0,0,1,0,0,  0,0,0,50,0,0},
-        {-1,0,0,1,0,0,  0,0,0,50,0,-1}
-    };*/
+    myFile.open("C:/Users/Living Room/Desktop/Manifest.txt", std::ios::in);
 
-    
-    tempNode = newNode(shipGrid,nullptr);
-    tempNode->cost = 0;
-    //printGrid(tempNode);
-    std::cout<<"\n";
-
-    if(isBalanced(tempNode))
+    if(myFile.is_open())
     {
-        std::cout<<"Ship is balanced\n";
+        std::string line;
+        while(getline(myFile,line))
+        {
+            yCoord.assign(line, 1, 2);
+            posY = std::stoi(yCoord); //Y coord
+
+            xCoord.assign(line, 4, 2);
+            posX = std::stoi(xCoord); // X coord
+
+            name.assign(line, 18, std::string::npos);
+
+            if (name == "NAN")
+            {
+                weight = -1;
+            }
+            else
+            {
+                getWeight.assign(line, 10, 5);
+                weight = std::stod(getWeight); //weight of container
+            }
+
+            if (name != "UNUSED" && name !="NAN")
+            {
+                shipGrid[(7-posY)+1][posX-1]->weight = weight;
+                shipGrid[(7-posY)+1][posX-1]->name.assign(name);
+                check.push_back(weight);
+            }
+            if (name == "NAN")
+            {
+                shipGrid[(7-posY)+1][posX-1]->weight = -1;
+                shipGrid[(7-posY)+1][posX-1]->name.assign("NAN");
+            }
+        }
+        myFile.close();
     }
     else
     {
-        std::cout<<"Ship is not balanced\n\n";
-        makeBalanced(tempNode);
-        
+        std::cout<<"\nFile Not found\n";
     }
-    
+
+    tempNode = newNode(shipGrid,nullptr);
+    tempNode->cost = 0;
+    printGrid(tempNode);
+    std::cout<<"\n";
+
+    reduce = std::accumulate(check.begin(), check.end(), reduce);
+    lowerBound = std::ceil((reduce/2.0)*.95);
+    upperBound = std::floor(((reduce/2.0)*1.05));
+    checksize = check.size();
+
+    for (int i = lowerBound; i < upperBound; i++)
+    {
+        if (isSubsetSum(check,checksize, i))
+        {
+            performSift = false;
+        }
+    }
+
+    if (checksize < 2)
+    {
+        performSift = true;
+    }
+
+    if (performSift == false)
+    {
+        if(isBalanced(tempNode))
+        {
+            std::cout<<"Ship is already balanced.\n";
+        }
+        else
+        {
+            std::cout<<"Ship is not balanced.\nCalculating steps to balance the ship:";
+            makeBalanced(tempNode, false);
+        }
+    }
+    else
+    {
+        std::cout<<"\nPerforming Sift\n";
+        makeBalanced(tempNode, true);
+    }
     return 0;
 }
 
 
-/*
-for (itr = dupeTrack.begin(); itr != dupeTrack.end(); itr++)
-    {
-        testOutput = *itr;
-
-        count++;
-    
-        for (int i = 0; i <testOutput.size();i++)
-        {
-            for (int j = 0; j < testOutput[i].size(); j ++)
-            {
-                std::cout<<testOutput[i][j]->weight<<" ";
-            }
-
-            std::cout<<"\n";
-        }
-
-        std::cout<<"\n";
-    }
-    
-    std::cout<<count;
-*/
